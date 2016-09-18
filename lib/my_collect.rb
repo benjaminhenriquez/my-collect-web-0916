112e71cb5a1aa61fc3ed7a4360b0569ff256e4b8
Benjamin Henriquez <henriquez.ben@gmail.com>
2016-09-18 08:47:34 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect (array)# put argument(s) here
+  # code here
+  new_array = []
+  count = 0
+  while count < array.length
+    new_array.push(yield array[count])
+    count+=1
+  end
+  new_array
+end
